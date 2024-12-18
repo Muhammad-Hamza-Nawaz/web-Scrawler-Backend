a027d9d563c4ed0f5d02e183f302fb7e6ac667af
Muhammad-Hamza-Nawaz <wildalbatross12gmail.com>
2024-12-19 00:25:27 +0500

    Replaced require with import for ES module support

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const express = require("express");
-const axios = require("axios");
-const cheerio = require("cheerio");
-const cors= require("cors")
+import express from 'express';
+import axios from 'axios';
+import cheerio from 'cheerio';
+import cors from("cors")
 const app = express();
 const PORT = 3000;
 app.use(cors());
